2331af67f9905fc6f3bfb2690fbd11e3e5795ef1
Adrian Werle <adrianwerle@live.com.ar>
2024-11-13 11:14:06 -0300

    primer_commit
    Primer commit

---
+++ mqtt_client/mqtt_client.ino
@@ -0,0 +1,171 @@
+#include <WiFiNINA.h>
+#include <PubSubClient.h>
+#include <Arduino.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+// Detalles de la red Wi-Fi
+const char* ssid = "VPA2";                 // SSID
+const char* password = "V@1ent1n@2OO3";    // Contraseña WiFi
+
+// Detalles del broker MQTT
+const char* mqttServer = "lnxsvr2";   // Dirección IP del broker MQTT
+const int mqttPort = 1883;                 // Puerto del broker MQTT
+const char* mqttUser = "usuario1";         // Usuario MQTT
+const char* mqttPassword = "clave1";       // Contraseña MQTT
+
+// Nombre de la placa
+const char* nameBoard = "mkr-1";
+
+// Crear las instancias necesarias
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+WiFiUDP udp; // Crea un objeto para el UDP
+NTPClient timeClient(udp, "pool.ntp.org", -10800, 60000); // Servidor NTP, zona horaria (UTC), intervalo de actualización (60 seg)
+
+unsigned long previousMillis = 0; // Almacena el tiempo anterior para el mensaje heartbeat
+const long interval = 60000; // Intervalo de tiempo de 10 segundos
+
+unsigned long previousDataMillis = 0; // Almacena el tiempo anterior para el mensaje de datos
+const long dataInterval = 10000; // Intervalo de tiempo para el mensaje de datos (10 segundos)
+
+String uid;
+
+void setup() {
+  Serial.begin(9600);
+
+  connectToWiFi();
+  
+  // Configura el servidor MQTT
+  mqttClient.setServer(mqttServer, mqttPort);
+
+  // Obtén la dirección MAC
+  uint8_t mac[6]; // Arreglo para almacenar la dirección MAC
+  WiFi.macAddress(mac);
+
+  for (int i = 0; i < 6; i++) {
+    // Convierte el octeto a hexadecimal
+    String octet = String(mac[i], HEX); 
+    // Asegúrate de que tenga dos caracteres, añadiendo un '0' al principio si es necesario
+    if (octet.length() == 1) {
+      octet = "0" + octet; // Completa con '0' si solo tiene un dígito
+    }
+    uid += octet; // Añade el octeto formateado a uid
+  }
+  uid.toUpperCase();
+}
+
+void loop() {
+  // Asegúrate de que el cliente está conectado
+  if (!mqttClient.connected()) {
+    reconnectMQTT();
+  }
+  
+  mqttClient.loop(); // Mantiene la conexión
+
+  // Actualiza la hora desde el servidor NTP
+  timeClient.update();
+
+  // Envía mensaje heartbeat cada 1 minuto
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    sendHeartbeat();
+  }
+
+    // Envía mensaje DATA cada 10 segundos
+  if (currentMillis - previousDataMillis >= dataInterval) {
+    previousDataMillis = currentMillis;
+    sendData();
+  }
+
+}
+
+void connectToWiFi() {
+  Serial.print("Conectando a WiFi...");
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  
+  Serial.println("Conectado a WiFi.");
+  Serial.print("Dirección IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnectMQTT() {
+  // Reintenta conectarse al broker MQTT
+  while (!mqttClient.connected()) {
+    Serial.print("Intentando conectar al MQTT...");
+
+    // Intento de conexión
+    if (mqttClient.connect("ClientID", mqttUser, mqttPassword)) {
+      Serial.println("conectado.");
+    } else {
+      Serial.print("Error en conexión MQTT, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" intentando de nuevo en 2 segundos.");
+      delay(2000);
+    }
+  }
+}
+
+void sendHeartbeat() {
+  // Obtiene el timestamp actual
+  unsigned long epochTime = timeClient.getEpochTime(); // Obtiene la hora formateada
+  // Convierte el epochTime a un String y concatena '000'
+  String timestampString = String(epochTime) + "000";
+
+  char message[150]; // Buffer para el mensaje
+
+  // Obtiene la dirección IP
+  IPAddress ip = WiFi.localIP();
+  String ipString = String(ip[0]) + "." + String(ip[1]) + "." + String(ip[2]) + "." + String(ip[3]);
+
+  // Obtiene el valor RSSI
+  long rssi = WiFi.RSSI();
+
+  // Crea el mensaje con el timestamp
+  snprintf(message, sizeof(message), 
+           "{\"timestamp\": \"%s\", \"name\": \"%s\", \"rssi\": \"%ld\", \"ip\": \"%s\"}", 
+           timestampString.c_str(), nameBoard, rssi, ipString.c_str());
+  
+  // Construye el tópico con el UID
+  String topic = "HEARTBEAT/" + uid;
+
+  // Publica en el topic "heartbeat"
+  mqttClient.publish(topic.c_str(), message); 
+  Serial.print("Mensaje enviado: ");
+  Serial.print(topic);
+  Serial.print(": ");
+  Serial.println(message);
+}
+
+void sendData() {
+  // Obtiene el timestamp actual
+  unsigned long epochTime = timeClient.getEpochTime(); // Obtiene la hora formateada
+  // Convierte el epochTime a un String y concatena '000'
+  String timestampString = String(epochTime) + "000";
+
+  char message[150]; // Buffer para el mensaje
+
+  // Genera un valor aleatorio entre 0 y 100
+  int randomValue = random(0, 101); // El rango es 0 a 100, el 101 es exclusivo
+
+  // Crea el mensaje con el timestamp
+  snprintf(message, sizeof(message), 
+           "{\"timestamp\": \"%s\", \"value\": \"%d\"}", 
+           timestampString.c_str(), randomValue);
+  
+  // Construye el tópico con el UID
+  String topic = "DATA/" + uid;
+
+  // Publica en el topic "heartbeat"
+  mqttClient.publish(topic.c_str(), message); 
+  Serial.print("Mensaje enviado: ");
+  Serial.print(topic);
+  Serial.print(": ");
+  Serial.println(message);
+}
